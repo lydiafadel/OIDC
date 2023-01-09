d3e0105f5fb3ed6df50a7d54f4e2a31da15f9e18
lydia <105000328+lydiafadel@users.noreply.github.com>
2023-01-09 10:21:30 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,15 @@
 output "ROLE_ARN"
   value       = module.oidc.ROLE_ARN
   description = "role"
-data.aws_iam_openid_connect_provider.arn
+    
+
+output "tfc_workload_identity_role_arn" {
+  value       = module.tfc_workload_identity_role.iam_role_arn
+  description = "IAM Role ARN for TFC Workload Identity"
+}
+  
+ output "tfc_workload_identity_audience" {
+  description = "Audience value for TFC workload identity"
+  value       = var.create_tfc_oidc_provider ? aws_iam_openid_connect_provider.tfc_provider[0].client_id_list : []
+}
+
